9d8ad596c5bfdef7daee061b1f126fc715070459
Andrew Swift <andrew.swift@norwegian.com>
2017-10-03 19:31:27 +0200

    Added ruter sanntid endpoint for lametric time integration

--- server/app.js
+++ server/app.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var https = require('https');
+var time = require('time')(Date);
+var moment = require('moment')
+var sanntid = require('sanntid'),
+    allLocations = sanntid.list();
+    
+
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use( function(req, res, next) {
@@ -13,6 +19,41 @@ app.use( function(req, res, next) {
 
 app.get('/', function (req, res) {
   res.send('Hello');
+});
+
+app.get('/sanntid', function (req, res) {
+    locations = sanntid.search(req.query.location);
+
+    if(locations.length == 0){
+      res.json({frames:[{text:"Not Found", icon : "i555"}]})
+      return;
+    }
+
+    sanntid.getRealtimeData(locations[0], function (data) {
+         res.setHeader('Content-Type', 'application/json');
+         
+         var details = data.map(function(x){ 
+           var timestamp = 	x.timestamp;
+           var now = new time.Date();
+           timestamp.setTimezone("Europe/Oslo");
+           now.setTimezone("Europe/Oslo")
+
+           var arrival = timestamp.getHours() + ':' + timestamp.getMinutes();
+
+           if ((timestamp.getMinutes() - now.getMinutes()) < 10) {
+            arrival = moment(timestamp).fromNow();
+           }  
+         
+           return {
+             text: x.line + " " + x.name + " " + arrival, icon : "a5387" }
+            }
+         );
+         var realTime = {
+            frames: details
+         }         
+         res.json(realTime);
+    });
+
 });
 
 // create application/json parser
